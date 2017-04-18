e3cf8856b1926810a31bb5f25807089c1f4574ec
Pierre Burgy <pierre.burgy@gmail.com>
2017-04-18 15:05:09 +0200

    Fix Promise.defer issue

--- files/api/upload/services/upload.js
+++ files/api/upload/services/upload.js
@@ -22,39 +22,35 @@ module.exports = {
    */
 
   upload: function * (part, ctx) {
+    return new Promise(function *(resolve, reject) {
+      // Init and format variables.
+      const promises = [];
+      const defaultUploadFolder = 'public/upload';
+      let stream;
+      ctx = ctx || {};
 
-    // Init the promise.
-    const deferred = Promise.defer();
+      // Set the filename.
+      const filename = Date.now().toString() + '-' + (_.kebabCase(part.filename) || Math.floor(Math.random() * 1000000).toString());
 
-    // Init and format variables.
-    const promises = [];
-    const defaultUploadFolder = 'public/upload';
-    let stream;
-    ctx = ctx || {};
+      // Start uploading.
+      stream = fs.createWriteStream(path.join(process.cwd(), strapi.api.upload.config.folder || defaultUploadFolder, filename));
+      part.pipe(stream);
 
-    // Set the filename.
-    const filename = Date.now().toString() + '-' + (_.kebabCase(part.filename) || Math.floor(Math.random() * 1000000).toString());
+      // Register the data of the file in the database.
+      promises.push(strapi.orm.collections.upload.create(_.merge(part, {
+        createdBy: ctx.user && ctx.user.id,
+        originalFilenameFormatted: _.kebabCase(part.filename),
+        originalFilename: part.filename || '',
+        filename: filename
+      })));
 
-    // Start uploading.
-    stream = fs.createWriteStream(path.join(process.cwd(), strapi.api.upload.config.folder || defaultUploadFolder, filename));
-    part.pipe(stream);
-
-    // Register the data of the file in the database.
-    promises.push(strapi.orm.collections.upload.create(_.merge(part, {
-      createdBy: ctx.user && ctx.user.id,
-      originalFilenameFormatted: _.kebabCase(part.filename),
-      originalFilename: part.filename || '',
-      filename: filename
-    })));
-
-    try {
-      const uploadDescriptions = yield promises;
-      deferred.resolve(uploadDescriptions);
-    } catch (err) {
-      strapi.log.error(err);
-      deferred.reject(err);
-    }
-
-    return deferred.promise;
+      try {
+        const uploadDescriptions = yield promises;
+        resolve(uploadDescriptions);
+      } catch (err) {
+        strapi.log.error(err);
+        reject(err);
+      }
+    });
   }
 };
